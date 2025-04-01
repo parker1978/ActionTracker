19346eafdfbe400948eb4b8673dc01da98bda485
Stephen Parker <parker1978@me.com>
2025-04-01 16:33:19 -0500

    Initial Commit

---
+++ ActionTracker/ActionTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActionTrackerApp.swift
+//  ActionTracker
+//
+//  Created by Stephen Parker on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ActionTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ActionTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ActionTracker
+//
+//  Created by Stephen Parker on 4/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
